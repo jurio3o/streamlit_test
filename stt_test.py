648b25dffee5817ac6e6967c705fb0ffc47fc3c3
jurio3o <hongjury@gmail.com>
2022-12-31 17:18:10 +0900

    test

--- stt_test.py
+++ stt_test.py
@@ -21,49 +21,49 @@ logger = logging.getLogger(__name__)
 
  
 # This code is based on https://github.com/streamlit/demo-self-driving/blob/230245391f2dda0cb464008195a470751c01770b/streamlit_app.py#L48  # noqa: E501
-def download_file(url, download_to: Path, expected_size=None):
-    # Don't download the file twice.
-    # (If possible, verify the download using the file length.)
-    if download_to.exists():
-        if expected_size:
-            if download_to.stat().st_size == expected_size:
-                return
-        else:
-            st.info(f"{url} is already downloaded.")
-            if not st.button("Download again?"):
-                return
+# def download_file(url, download_to: Path, expected_size=None):
+#     # Don't download the file twice.
+#     # (If possible, verify the download using the file length.)
+#     if download_to.exists():
+#         if expected_size:
+#             if download_to.stat().st_size == expected_size:
+#                 return
+#         else:
+#             st.info(f"{url} is already downloaded.")
+#             if not st.button("Download again?"):
+#                 return
 
-    download_to.parent.mkdir(parents=True, exist_ok=True)
+#     download_to.parent.mkdir(parents=True, exist_ok=True)
 
-    # These are handles to two visual elements to animate.
-    weights_warning, progress_bar = None, None
-    try:
-        weights_warning = st.warning("Downloading %s..." % url)
-        progress_bar = st.progress(0)
-        with open(download_to, "wb") as output_file:
-            with urllib.request.urlopen(url) as response:
-                length = int(response.info()["Content-Length"])
-                counter = 0.0
-                MEGABYTES = 2.0 ** 20.0
-                while True:
-                    data = response.read(8192)
-                    if not data:
-                        break
-                    counter += len(data)
-                    output_file.write(data)
+#     # These are handles to two visual elements to animate.
+#     weights_warning, progress_bar = None, None
+#     try:
+#         weights_warning = st.warning("Downloading %s..." % url)
+#         progress_bar = st.progress(0)
+#         with open(download_to, "wb") as output_file:
+#             with urllib.request.urlopen(url) as response:
+#                 length = int(response.info()["Content-Length"])
+#                 counter = 0.0
+#                 MEGABYTES = 2.0 ** 20.0
+#                 while True:
+#                     data = response.read(8192)
+#                     if not data:
+#                         break
+#                     counter += len(data)
+#                     output_file.write(data)
 
-                    # We perform animation by overwriting the elements.
-                    weights_warning.warning(
-                        "Downloading %s... (%6.2f/%6.2f MB)"
-                        % (url, counter / MEGABYTES, length / MEGABYTES)
-                    )
-                    progress_bar.progress(min(counter / length, 1.0))
-    # Finally, we remove these visual elements by calling .empty().
-    finally:
-        if weights_warning is not None:
-            weights_warning.empty()
-        if progress_bar is not None:
-            progress_bar.empty()
+#                     # We perform animation by overwriting the elements.
+#                     weights_warning.warning(
+#                         "Downloading %s... (%6.2f/%6.2f MB)"
+#                         % (url, counter / MEGABYTES, length / MEGABYTES)
+#                     )
+#                     progress_bar.progress(min(counter / length, 1.0))
+#     # Finally, we remove these visual elements by calling .empty().
+#     finally:
+#         if weights_warning is not None:
+#             weights_warning.empty()
+#         if progress_bar is not None:
+#             progress_bar.empty()
 
 
 def main():
@@ -74,24 +74,20 @@ ViTo STT 적용을 위한 실험 페이지
 """
     )
 
-    # # https://github.com/mozilla/DeepSpeech/releases/tag/v0.9.3
+    # https://github.com/mozilla/DeepSpeech/releases/tag/v0.9.3
     # MODEL_URL = "https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm"  # noqa
     # LANG_MODEL_URL = "https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer"  # noqa
     # MODEL_LOCAL_PATH = HERE / "models/deepspeech-0.9.3-models.pbmm"
     # LANG_MODEL_LOCAL_PATH = HERE / "models/deepspeech-0.9.3-models.scorer"
 
-    #모델 다운로드 할 필요 없음 
     # download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=188915987)
     # download_file(LANG_MODEL_URL, LANG_MODEL_LOCAL_PATH, expected_size=953363776)
 
-    # 모델 parameter
     # lm_alpha = 0.931289039105002
     # lm_beta = 1.1834137581510284
     # beam = 100
 
-    app_sst(
-        # str(MODEL_LOCAL_PATH), str(LANG_MODEL_LOCAL_PATH), lm_alpha, lm_beta, beam
-    )
+    app_sst()
 
 
 
@@ -123,14 +119,11 @@ def app_sst():
                 # model.setScorerAlphaBeta(lm_alpha, lm_beta)
                 # model.setBeamWidth(beam)
 
-                #stream을 생성해준다?? 
                 # stream = model.createStream()
 
                 status_indicator.write("Model loaded.")
 
             sound_chunk = pydub.AudioSegment.empty()
-
-            
             try:
                 audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
             except queue.Empty:
@@ -141,25 +134,34 @@ def app_sst():
             status_indicator.write("Running. Say something!")
 
             for audio_frame in audio_frames:
+                # sound에 브라우저를 통해 녹음하는 소리가 저장됨
+                # text_output.markdown(f"========================")
+                # text_output.markdown(f"audio_frame {type(audio_frame)}")
                 sound = pydub.AudioSegment(
                     data=audio_frame.to_ndarray().tobytes(),
                     sample_width=audio_frame.format.bytes,
                     frame_rate=audio_frame.sample_rate,
                     channels=len(audio_frame.layout.channels),
                 )
+                # 실시간으로 녹음되는 것들을 합치는 과정인듯
+                # text_output.markdown(f"sound {type(sound)}")
                 sound_chunk += sound
-                print(sound)
-
+            
             if len(sound_chunk) > 0:
-                sound_chunk = sound_chunk.set_channels(1).set_frame_rate(
-                    16000
-                    # model.sampleRate()
-                )
+                # set_channels(1): 인자값이 1이면 모노, 2면 스테레오라는데,,, 잘 모르겠음 쨌든 기본 값은 1이라 그대로 유지
+                # set_frame_rate(): 음질의 품질을 이야기 하는 것 같은데,, 잘 모르겠네
+                # text_output.markdown(f"sound_chunk: {type(sound_chunk)}")
+                sound_chunk = sound_chunk.set_channels(1)
+                # text_output.markdown(f"sound_chunk: {type(sound_chunk)}")
+                
 
-                # buffer = np.array(sound_chunk.get_array_of_samples())
-                # stream.feedAudioContent(buffer)
-                # text = stream.intermediateDecode()
-                # text_output.markdown(f"**Text:** {text}")
+                buffer = np.array(sound_chunk.get_array_of_samples())
+                # buffer 상태 확인
+                text_output.markdown(f"buffer: {type(buffer)}")
+                # stream: 모델에 
+                stream.feedAudioContent(buffer)
+                text = stream.intermediateDecode()
+                text_output.markdown(f"**Text:** {text}")
         else:
             status_indicator.write("AudioReciver is not set. Abort.")
             break

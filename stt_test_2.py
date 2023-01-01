65ca838ca08cd0643aefb9dcec511386bc5050c7
jurio3o <hongjury@gmail.com>
2023-01-02 01:04:23 +0900

    test

--- stt_test_2.py
+++ stt_test_2.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from synthesizer.inference import Synthesizer
-from encoder import inference as encoder
-from vocoder import inference as vocoder
+# from synthesizer.inference import Synthesizer
+# from encoder import inference as encoder
+# from vocoder import inference as vocoder
 from pathlib import Path
 import numpy as np
 import soundfile as sf
@@ -12,21 +12,21 @@ from helper import draw_embed, create_spectrogram, read_audio, record, save_reco
 
 "# Streamlit showcase"
 
-model_load_state = st.text("Loading pretrained models...")
+# model_load_state = st.text("Loading pretrained models...")
 
-seed = 42
-low_mem = False
-num_generated = 0
-enc_model_fpath = Path("encoder/saved_models/pretrained.pt")
-syn_model_dir = Path("synthesizer/saved_models/logs-pretrained/")
-voc_model_fpath = Path("vocoder/saved_models/pretrained/pretrained.pt")
-encoder.load_model(enc_model_fpath)
-synthesizer = Synthesizer(
-    syn_model_dir.joinpath("taco_pretrained"), low_mem=low_mem, seed=seed
-)
-vocoder.load_model(voc_model_fpath)
+# seed = 42
+# low_mem = False
+# num_generated = 0
+# enc_model_fpath = Path("encoder/saved_models/pretrained.pt")
+# syn_model_dir = Path("synthesizer/saved_models/logs-pretrained/")
+# voc_model_fpath = Path("vocoder/saved_models/pretrained/pretrained.pt")
+# encoder.load_model(enc_model_fpath)
+# synthesizer = Synthesizer(
+#     syn_model_dir.joinpath("taco_pretrained"), low_mem=low_mem, seed=seed
+# )
+# vocoder.load_model(voc_model_fpath)
 
-model_load_state.text("Loaded pretrained models!")
+# model_load_state.text("Loaded pretrained models!")
 
 st.header("1. Record your own voice")
 
@@ -52,66 +52,66 @@ if st.button(f"Click to Record"):
         fig = create_spectrogram(path_myrecording)
         st.pyplot(fig)
 
-"## 2. Choose an audio record"
+# "## 2. Choose an audio record"
 
-audio_folder = "samples"
-filenames = glob.glob(os.path.join(audio_folder, "*.mp3"))
-selected_filename = st.selectbox("Select a file", filenames)
+# audio_folder = "samples"
+# filenames = glob.glob(os.path.join(audio_folder, "*.mp3"))
+# selected_filename = st.selectbox("Select a file", filenames)
 
-if selected_filename is not None:
-    # Create embedding
-    in_fpath = Path(selected_filename.replace('"', "").replace("'", ""))
-    original_wav, sampling_rate = librosa.load(str(in_fpath))
-    preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
-    embed = encoder.embed_utterance(preprocessed_wav)
-    st.success("Created the embedding")
+# if selected_filename is not None:
+#     # Create embedding
+#     in_fpath = Path(selected_filename.replace('"', "").replace("'", ""))
+#     original_wav, sampling_rate = librosa.load(str(in_fpath))
+#     preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
+#     embed = encoder.embed_utterance(preprocessed_wav)
+#     st.success("Created the embedding")
 
-    st.audio(read_audio(in_fpath))
+#     st.audio(read_audio(in_fpath))
 
-    if st.sidebar.checkbox("Do you want to change your embedding?"):
-        height = int(np.sqrt(len(embed)))
-        shape = (height, -1)
-        matrix_embed = np.round(embed, 2).reshape(shape)
-        matrix_embed = [list(row) for row in matrix_embed]
-        a = st.text_area("Change your embedding:", value=str(matrix_embed).replace("],", "],\n"))
+#     if st.sidebar.checkbox("Do you want to change your embedding?"):
+#         height = int(np.sqrt(len(embed)))
+#         shape = (height, -1)
+#         matrix_embed = np.round(embed, 2).reshape(shape)
+#         matrix_embed = [list(row) for row in matrix_embed]
+#         a = st.text_area("Change your embedding:", value=str(matrix_embed).replace("],", "],\n"))
 
-        matrix = [[float(x) for x in row.strip("[] \n").split(",")] for row in a.split("],")]
-        embed = np.array(matrix).flatten()
+#         matrix = [[float(x) for x in row.strip("[] \n").split(",")] for row in a.split("],")]
+#         embed = np.array(matrix).flatten()
 
-    fig = draw_embed(embed, "myembedding", None)
-    st.pyplot(fig)
-
-
-"## 3. Synthesize text."
-text = st.text_input("Write a sentence (+-20 words) to be synthesized:")
+#     fig = draw_embed(embed, "myembedding", None)
+#     st.pyplot(fig)
 
 
-def pgbar(i, seq_len, b_size, gen_rate):
-    mybar.progress(i / seq_len)
+# "## 3. Synthesize text."
+# text = st.text_input("Write a sentence (+-20 words) to be synthesized:")
 
 
-if st.button("Click to synthesize"):
-    texts = [text]
-    embeds = [embed]
+# def pgbar(i, seq_len, b_size, gen_rate):
+#     mybar.progress(i / seq_len)
 
-    # generate waveform
-    with st.spinner("Generating your speech..."):
-        specs = synthesizer.synthesize_spectrograms(texts, embeds)
-        spec = specs[0]
-        synthesize_state = st.text("Created the mel spectrogram")
-        synthesize_state.text("Generating the waveform...")
-        mybar = st.progress(0)
-        generated_wav = vocoder.infer_waveform(spec, progress_callback=pgbar)
-        generated_wav = np.pad(
-            generated_wav, (0, synthesizer.sample_rate), mode="constant"
-        )
-        generated_wav = encoder.preprocess_wav(generated_wav)
-        synthesize_state.text("Synthesized the waveform")
-        st.success("Done!")
 
-    # Save it on the disk
-    filename = "demo_output_%02d.wav" % num_generated
-    sf.write(filename, generated_wav.astype(np.float32), synthesizer.sample_rate)
-    num_generated += 1
-    synthesize_state.text("\nSaved output as %s\n\n" % filename)
-    st.audio(read_audio(filename))
+# if st.button("Click to synthesize"):
+#     texts = [text]
+#     embeds = [embed]
+
+#     # generate waveform
+#     with st.spinner("Generating your speech..."):
+#         specs = synthesizer.synthesize_spectrograms(texts, embeds)
+#         spec = specs[0]
+#         synthesize_state = st.text("Created the mel spectrogram")
+#         synthesize_state.text("Generating the waveform...")
+#         mybar = st.progress(0)
+#         generated_wav = vocoder.infer_waveform(spec, progress_callback=pgbar)
+#         generated_wav = np.pad(
+#             generated_wav, (0, synthesizer.sample_rate), mode="constant"
+#         )
+#         generated_wav = encoder.preprocess_wav(generated_wav)
+#         synthesize_state.text("Synthesized the waveform")
+#         st.success("Done!")
+
+#     # Save it on the disk
+#     filename = "demo_output_%02d.wav" % num_generated
+#     sf.write(filename, generated_wav.astype(np.float32), synthesizer.sample_rate)
+#     num_generated += 1
+#     synthesize_state.text("\nSaved output as %s\n\n" % filename)
+#     st.audio(read_audio(filename))

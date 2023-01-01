5795de45b5d46fdb6e24b16eeb23bb741db19dff
jurio3o <hongjury@gmail.com>
2023-01-02 01:30:42 +0900

    test

--- stt_test_2.py
+++ stt_test_2.py
@@ -3,4 +3,8 @@ from audio_recorder_streamlit import audio_recorder
 
 audio_bytes = audio_recorder()
 if audio_bytes:
-    st.audio(audio_bytes, format="audio/wav")
+    audio = st.audio(audio_bytes, format="audio/wav")
+    audio
+    
+    if st.download_button('Download file', audio):
+        st.write('thank you')

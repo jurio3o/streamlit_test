883e03ef1eda7f4e5e02cfb1c42290dce5742e2f
jurio3o <hongjury@gmail.com>
2023-01-02 01:33:47 +0900

    test

--- stt_test_2.py
+++ stt_test_2.py
@@ -3,9 +3,8 @@ from audio_recorder_streamlit import audio_recorder
 
 audio_bytes = audio_recorder()
 
-if audio_bytes:
+if audio_bytes: # audio_bytes에 음성이 녹음됨! => 지금은 자동으로 녹음,, 소리가 끊어지면 바로 녹음 중단.
     st.audio(audio_bytes, format="audio/wav")
 
-    
     if st.download_button('Download file', audio_bytes):
         st.write('thank you')

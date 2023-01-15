ecc3762e7cd1e1fed4be7e9f726adaa53f1cb9ea
jurio3o <102223289+jurio3o@users.noreply.github.com>
2023-01-16 00:48:12 +0900

    test

--- test.py
+++ test.py
@@ -1,13 +1,37 @@
 import streamlit as st
-from audiorecorder import audiorecorder
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
-st.title("Audio Recorder")
-audio = audiorecorder("Click to record", "Recording...")
+stt_button = Button(label="Speak", width=100)
 
-if len(audio) > 0:
-    # To play audio in frontend:
-    st.audio(audio.tobytes())
-    
-    # To save audio to a file:
-    wav_file = open("audio.mp3", "wb")
-    wav_file.write(audio.tobytes())
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+ 
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_TEXT" in result:
+        st.write(result.get("GET_TEXT"))

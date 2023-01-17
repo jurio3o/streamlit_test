0ac94603618dd767f1388fd29b06013c59429e36
nineone-dora <juri@nine2one.io>
2023-01-17 19:59:19 +0900

    test

--- test.py
+++ test.py
@@ -1,43 +1,49 @@
 import streamlit as st
-# from bokeh.models.widgets import Button
-# from bokeh.models import CustomJS
-# from streamlit_bokeh_events import streamlit_bokeh_events
-import recorder
+from audio_recorder_streamlit import audio_recorder
+from io import BytesIO
+from pydub import AudioSegment
+import speech_recognition as sr
 
-# stt_button = Button(label="Speak", width=100)
-if st.button('start'):
-    stream_params = recorder.StreamParams()
-    recorder = recorder.Recorder(stream_params)
-    st.write(recorder.record(5, "audio.wav"))
-    
+# import numpy as np
+# from scipy.io.wavfile import write
+# import soundfile as sf
+# import wave
+# import streamlit.components.v1 as components
+# import os
 
-# stt_button.js_on_event("button_click", CustomJS(code="""
-#     var recognition = new webkitSpeechRecognition();
-#     recognition.continuous = true;
-#     recognition.interimResults = true;
- 
-#     recognition.onresult = function (e) {
-#         var value = "";
-#         for (var i = e.resultIndex; i < e.results.length; ++i) {
-#             if (e.results[i].isFinal) {
-#                 value += e.results[i][0].transcript;
-#             }
-#         }
-#         if ( value != "") {
-#             document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-#         }
-#     }
-#     recognition.start();
-#     """))
+# sound_chunk = pydub.AudioSegment.empty()
 
-# result = streamlit_bokeh_events(
-#     stt_button,
-#     events="GET_TEXT",
-#     key="listen",
-#     refresh_on_update=False,
-#     override_height=75,
-#     debounce_time=0)
+# for audio_frame in audio_bytes:
+#     sound = pydub.AudioSegment(
+#                         data=audio_frame.to_ndarray().tobytes(),
+#                         sample_width=audio_frame.format.bytes,
+#                         frame_rate=audio_frame.sample_rate,
+#                         channels=len(audio_frame.layout.channels),
+#                     )
+#     sound_chunk += sound
 
-# if result:
-#     if "GET_TEXT" in result:
-#         st.write(result.get("GET_TEXT"))
+# sound.export('./testttt.wav', format='wav')
+st.title("Audio Recorder")
+
+
+audio_bytes = audio_recorder("Click to record", pause_threshold=10.0)
+if audio_bytes:
+    st.audio(audio_bytes, format="audio/wav")
+
+AudioSegment.from_raw(BytesIO(audio_bytes), sample_width=2, frame_rate=32000, channels=2).export('./testttt.wav', format='wav')
+# wav_file = open("audio.wav", "wb")
+# wav_file.write(audio_bytes.tobytes()).export('./asdf.wav', format = 'wav')
+
+r = sr.Recognizer()
+
+test = sr.AudioFile('./testttt.wav')
+with test as source:
+    audio = r.record(source)
+
+
+result = r.recognize_google(audio, language = 'ko-KR', show_all=True)['alternative']
+rr = []
+for i in result:
+    rr.append(i['transcript'])
+rr = '.'.join(rr)
+st.markdown(f'{rr}')
